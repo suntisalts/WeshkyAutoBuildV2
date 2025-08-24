c7c637d39869c5e6b995c34a188fe1058d155130
suntisalts <suntisalts@gmail.com>
2025-08-25 01:13:47 +0200

    Update libraryTest.lua

--- Server/libraryTest.lua
+++ Server/libraryTest.lua
@@ -11,7 +11,7 @@ if game:GetService("CoreGui"):FindFirstChild("imgui2") then
     game:GetService("CoreGui"):FindFirstChild("imgui2"):Destroy()
 end
 
-local tzu = Enum.Font.SourceSansBold 
+local tzu = Enum.Font.GothamSemibold 
 
 do -- Load items
     local imgui2 = Instance.new("ScreenGui")
