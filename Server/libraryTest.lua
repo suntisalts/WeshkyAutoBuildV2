193cac2851ad466404da34a472685fc9da83c267
suntisalts <suntisalts@gmail.com>
2025-08-25 02:50:31 +0200

    Update libraryTest.lua

--- Server/libraryTest.lua
+++ Server/libraryTest.lua
@@ -1442,7 +1442,7 @@ local library library = {
             tabbutton.Parent = tabbuttons
             tabbutton.Text = tabOptions.text
             tabbutton.Font = tzu3
-            tabbutton.TextSize = 14
+            tabbutton.TextSize = 15
             tabbutton.Size = UDim2.new(0, tabbutton.TextBounds.X, 1, 0)
             tabbutton.TextColor3 = Color3.new(0.4, 0.4, 0.4)
             tabbutton.MouseButton1Click:Connect(function()
